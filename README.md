676cf3774fbfd38662bd905a7e943c835099ac0c
Quentin Kniep <hello@quentinkniep.com>
2023-09-21 22:07:28 +0200

    added regex support and debug flag

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cypress
 
-Attach a progress bar to any CLI tool.
+Turn (almost) any streaming output into a progress bar.
 
 ## Usage
 
@@ -18,3 +18,15 @@ ping ::1 -c 10 | sed 's/.*seq=\([0-9]\+\).*/\1/' | cypress --min 1 --max 10
 
 Note that in this example the piped data contains a non-integer (empty string) line.
 This is ignored by Cypress, as would values outside the `--min` and `--max` range.
+
+Also, cypress supports regex parsing out of the box:
+
+```sh
+ping ::1 -c 10 | cypress --min 1 --max 10 --regex 'seq=(\d+)'
+```
+
+If you are having trouble getting the correct regex working, try adding the debug option for additional info on what happens during parsing:
+
+```sh
+ping ::1 -c 10 | cypress --min 1 --max 10 --regex 'seq=(\d+)' --debug
+```
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::{stdin, BufRead};
 use clap::Parser;
 use color_eyre::Result;
 use indicatif::ProgressStyle;
+use regex::Regex;
 
 /// Simple program to turn any output into a progress bar
 #[derive(Parser, Debug)]
@@ -19,6 +20,18 @@ struct Args {
     /// ...
     #[arg(long)]
     max: u64,
+
+    /// Regex applied to the input stream.
+    /// In `lines` mode: All lines not matching regex are ignored.
+    /// In `numbers` mode: If the regex matches,
+    /// content of first capture group is treated as the integer input.
+    #[arg(long)]
+    regex: Option<String>,
+
+    /// Prints out debug information.
+    /// This is especially useful for constructing regexes.
+    #[arg(long, default_value_t = false)]
+    debug: bool,
 }
 
 fn main() -> Result<()> {
@@ -37,19 +50,46 @@ fn main() -> Result<()> {
 
     let stdin = stdin();
     for line in stdin.lines() {
-        let line = line?;
+        let mut line = line?;
 
         if args.lines {
+            if let Some(re) = &args.regex {
+                if Regex::new(re).unwrap().is_match(&line) {
+                    progress.inc(1);
+                } else {
+                    if args.debug {
+                        eprintln!("Did not match on line: {}", line);
+                    }
+                }
+                continue;
+            }
+
             progress.inc(1);
             continue;
         }
 
+        if let Some(re) = &args.regex {
+            if let Some(captures) = Regex::new(re).unwrap().captures(&line) {
+                line = captures[1].to_string();
+            } else {
+                if args.debug {
+                    eprintln!("Did not match on line: {}", line);
+                }
+            }
+        }
+
         let res = line.parse();
         if res.is_err() {
+            if args.debug {
+                eprintln!("Failed to parse as int: {}", line);
+            }
             continue;
         }
         let number: u64 = res.unwrap();
         if number < args.min || number > args.max {
+            if args.debug {
+                eprintln!("Integer out of min/max bounds: {}", number);
+            }
             continue;
         }
         progress.set_position(number - args.min);
